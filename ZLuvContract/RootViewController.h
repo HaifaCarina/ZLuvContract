a14a4d2c7b330b8f06d73807fe85ecbd0c3288f6
Haifa <haifa@baluyos.net>
2012-04-13 18:29:04 +0800

    Displays frames/template selection

--- ZLuvContract/RootViewController.h
+++ ZLuvContract/RootViewController.h
@@ -10,7 +10,7 @@
 
 
 @interface RootViewController : UIViewController {
-    
+    UIScrollView *framesScrollView;
 }
 
 @end
--- ZLuvContract/RootViewController.m
+++ ZLuvContract/RootViewController.m
@@ -11,4 +11,37 @@
 
 @implementation RootViewController
 
+- (void) loadView {
+    [super loadView];
+    
+    NSMutableArray *framesArray = [[NSMutableArray alloc ]init];
+    [framesArray addObject:[UIImage imageNamed:@"1.jpg"]];
+    [framesArray addObject:[UIImage imageNamed:@"2.jpeg"]];
+    [framesArray addObject:[UIImage imageNamed:@"3.jpg"]];
+    [framesArray addObject:[UIImage imageNamed:@"4.jpg"]];
+    
+    
+    
+    framesScrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+    framesScrollView.scrollEnabled = YES;
+    framesScrollView.showsHorizontalScrollIndicator = TRUE;
+    framesScrollView.showsVerticalScrollIndicator = TRUE;
+    framesScrollView.contentSize = CGSizeMake(self.view.frame.size.width, [framesArray count] * 180);
+    
+    int yAxis = 0;
+    for (UIImage *img in framesArray) {
+        
+        UIImageView *imgView = [[UIImageView alloc] initWithFrame:CGRectMake(60, yAxis, 150, 150)];
+        imgView.image = img;
+        [framesScrollView addSubview:imgView];
+        [imgView release];
+        
+        yAxis += 170;
+        
+    }
+    
+    [self.view addSubview:framesScrollView];
+     
+    
+}
 @end
